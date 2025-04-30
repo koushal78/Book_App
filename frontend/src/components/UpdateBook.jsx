98328b03c95a39be41d358ea9186c3532f3f143c
Koushal kumar <kushwahakoushal78@gmail.com>
2025-04-30 12:10:09 +0530

    final push

--- frontend/src/components/UpdateBook.jsx
+++ frontend/src/components/UpdateBook.jsx
@@ -49,7 +49,7 @@ const UpdateBook = () => {
   const handleUpdate = async (e) => {
     e.preventDefault();
     try {
-      await axios.put(`http://localhost:9000/books/${selectedBookId}`, formData);
+      await axios.put(`https://book-app-6elb.onrender.com/books/${selectedBookId}`, formData);
       alert('Book updated successfully');
       fetchBooks();
     } catch (error) {
